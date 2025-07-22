92344959b5e6039d35cbfb086d286717bb330f6f
salorint <salorint@bwg-io.site>
2025-07-22 12:49:37 +0300

    Add first commit

---
+++ app/pkg/models/sqlalchemy_models/v1/user.py
@@ -0,0 +1,20 @@
+"""SQlAlchemy model for user"""
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+from uuid import UUID as UUIDType
+from typing import Optional
+
+from app.pkg.models.sqlalchemy_models import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[UUIDType] = mapped_column(UUID(as_uuid=True), primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    created_at: Mapped[datetime] = mapped_column(nullable=False)
+    updated_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
---
+++ app/pkg/models/v1/__init__.py
@@ -0,0 +1,2 @@
+
+from app.pkg.models.v1.app.user import *
